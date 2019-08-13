aa3632898f4c871e946168c049df6374cf1f7ddd
Fabian <fabrofabian@gmail.com>
2019-08-13 09:36:51 -0700

    Can select new scale

--- composerinspire-backend/app/controllers/scales_controller.rb
+++ composerinspire-backend/app/controllers/scales_controller.rb
@@ -4,6 +4,22 @@ class ScalesController < ApplicationController
         render json: scales
     end
 
+    def new
+        scale = Scale.new
+    end
+
+    def create
+        scale = Scale.new(scale_name: scale_params[:scale_name], composition_id: scale_params[:composition_id])
+        scale.save
+        render json: scale
+    end
+
+    def destroy
+        scale = Scale.find(params[:id])
+        scale.destroy
+        render json: scale
+    end
+
     private
     def scale_params
         params.require(:scale).permit(:scale_name, :composition_id)
